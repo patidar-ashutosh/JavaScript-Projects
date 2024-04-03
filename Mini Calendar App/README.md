d78858ddd5c9cca18c00d07b1b47fad8f2fabd32
patidar-ashutosh <patelashutosh2200@gmail.com>
2024-04-03 18:38:46 +0530

    Adding Mini Calendar App

---
+++ Mini Calendar App/README.md
@@ -0,0 +1,3 @@
+# This is Mini Calendar App Using HTML CSS And JavaScript.
+
+Project Link 👉 https://mini-calendar-app.vercel.app/
---
+++ Mini Calendar App/script.js
@@ -0,0 +1,16 @@
+let date = document.getElementById("date");
+let day = document.getElementById("day");
+let month = document.getElementById("month");
+let year = document.getElementById("year");
+
+const today = new Date();
+
+const weekDays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+const allMonths= ["January","February","March","April","May","June","July",
+"August","September","October","November","December"];
+
+date.innerHTML = (today.getDate() < 10 ? "0" : "" ) + today.getDate();
+day.innerHTML = weekDays[today.getDay()];
+month.innerHTML = allMonths[today.getMonth()];
+year.innerHTML = today.getFullYear();
